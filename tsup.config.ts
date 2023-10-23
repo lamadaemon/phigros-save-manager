fe82c5a8ed61617efeeb5d1f21cbe6834a0dd0a9
lamadaemon <145078438+lamadaemon@users.noreply.github.com>
2023-10-23 18:26:22 +0000

    fix cli.ts is not included in tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["index.ts"],
+  entry: ["index.ts", "cli.ts"],
   format: ["cjs", "esm"],
   dts: true,
   splitting: false,
